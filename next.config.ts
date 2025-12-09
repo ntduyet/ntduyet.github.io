2d16a1ac020cb4795be331994e96fa8021057a3f
duyetnt <duyetnt.se.uit@hotmail.com>
2025-12-09 13:04:10 +0700

    chore: add deploy action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",
+  basePath: "/ntduyet.github.io",
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
