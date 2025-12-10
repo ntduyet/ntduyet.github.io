5158098f1a996662ebdbc00a3712984611f33b96
Duyet Nguyen <104858620+ntduyet@users.noreply.github.com>
2025-12-10 12:30:25 +0700

    fix: improve responsiveness of header (#20)

--- app/ui/personal/personal-container.tsx
+++ app/ui/personal/personal-container.tsx
@@ -2,11 +2,26 @@ import PersonalDetails from "./personal-details";
 import PersonalMetadataList from "./personal-metadata-list";
 import { Person, Metadata } from "../../services/resume-loader";
 
-export default function PersonalContainer({ person, metadatas } : { person: Person, metadatas: Metadata[] }) {
+export default function PersonalContainer({
+  person,
+  metadatas,
+}: {
+  person: Person;
+  metadatas: Metadata[];
+}) {
   return (
-    <div className="personal-container grid grid-cols-[1fr_auto] items-start gap-[12px]">
+    <div className="personal-container grid grid-cols-[auto_auto] items-start gap-[12px]">
       <PersonalDetails person={person} />
-      <PersonalMetadataList metadatas={metadatas} align="right" />
+      <PersonalMetadataList
+        className="text-lef row-start-2 col-span-1 sm:hidden"
+        metadatas={metadatas}
+        align="left"
+      />
+      <PersonalMetadataList
+        className="text-right row-[1_/1_] sm:col-[2_/2_] hidden sm:grid"
+        metadatas={metadatas}
+        align="right"
+      />
     </div>
   );
 }
--- app/ui/personal/personal-metadata-list.tsx
+++ app/ui/personal/personal-metadata-list.tsx
@@ -3,13 +3,27 @@
 import { Metadata } from "../../services/resume-loader";
 import PersonalMetadata from "./personal-metadata";
 
-export default function PersonalMetadataList({ metadatas, align } : { metadatas: Metadata[], align: 'left' | 'right'}) {
+export default function PersonalMetadataList({ 
+  className, 
+  metadatas, 
+  align 
+} : { 
+  className?: 
+  string, 
+  metadatas: Metadata[],
+  align: 'left' | 'right' 
+}) {
   function metadataListItems(): React.ReactNode[] {
     return metadatas.map(i => <PersonalMetadata key={i.id} metadata={i} align={align} />)
   }
 
+  let finalClassName = 'personal-metadata-list w-full'
+  if (className !== undefined) {
+    finalClassName += ` ${className}`;
+  } 
+
   return (
-    <div className={`personal-metadata-list text-${align}`}>
+    <div className={finalClassName}>
       {metadataListItems()}
     </div>
   )
