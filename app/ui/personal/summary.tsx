a3df9e8ada73895d9d94bd606a7980396228b0e1
duyetnt <duyetnt.se.uit@hotmail.com>
2025-12-14 12:06:26 +0700

    chore: refactor summary to use FontAwesomeIcon instead

--- app/ui/personal/summary.tsx
+++ app/ui/personal/summary.tsx
@@ -1,7 +1,9 @@
+import FontAwesomeIcon from '@/app/ui/font-awesome-icon'
+
 export default function Summary({ content }: { content: string }) {
   return (
     <p className="flex items-start">
-      <span className="fa-sharp fa-solid fa-quote-left fa-lg mt-[6px] pr-[6px]" />
+      <FontAwesomeIcon type='quote' className="mt-[6px] pr-[6px]" />
       <i className="text-neutral-500">{content}</i>
     </p>
   )
