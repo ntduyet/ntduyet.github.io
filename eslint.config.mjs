c797434a8399a3f0736a4cca591218e2932c8663
duyetnt <duyetnt.se.uit@hotmail.com>
2026-03-22 16:17:04 +0700

    chore: initial cursor setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,16 @@
 // @ts-check
 
 import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strict,
-  tseslint.configs.stylistic, 
+  tseslint.configs.stylistic,
   {
-    ignores: [
-      ".next",
-      "./scripts/**",
-      "**/tests",
-    ]
-  }
+    ignores: ['.next', './scripts/**', '**/tests'],
+  },
+  eslintConfigPrettier,
 );
